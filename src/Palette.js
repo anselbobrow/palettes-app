a0c44888cbdcf1b3bee6bcf8b71d8e6efba4a142
Ansel Bobrow <anselbobrow@Ansels-MacBook-Pro.local>
2020-06-27 15:31:35 -0400

    create Palette, seedColors

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar */}
+        <div className="Palette-swatches">{/* Bunch of swatches */}</div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
