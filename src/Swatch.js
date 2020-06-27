8717400cd968c799c3730c0ac1f8c45dabd341f8
Ansel Bobrow <anselbobrow@Ansels-MacBook-Pro.local>
2020-06-27 16:49:02 -0400

    add copy overlay animation

--- src/Swatch.js
+++ src/Swatch.js
@@ -3,11 +3,32 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './Swatch.css';
 
 class Swatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1000);
+    });
+  }
+
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
-        <div style={{ background: background }} className="Swatch">
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ background }} className="Swatch">
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && 'show'}`}
+          ></div>
+          <div className={`copy-message ${copied && 'show'}`}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
