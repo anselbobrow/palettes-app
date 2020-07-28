c1f36bfa5145e11238ee6c9dbf927c2dfd5519fc
Ansel Bobrow <anselbobrow@Ansels-MacBook-Pro.local>
2020-07-28 01:09:04 -0400

    add jss for MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = {};
+const styles = {
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  colors: {},
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative',
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem',
+  },
+};
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   console.log(classes);
   return (
-    <div>
-      <h1 className={classes.main}>MiniPalette</h1>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
