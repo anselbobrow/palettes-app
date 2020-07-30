42484ff65882dacba41a27cbf50c96851b47a634
Ansel Bobrow <anselbobrow@Ansels-MacBook-Pro.local>
2020-07-30 16:25:47 -0400

    add go-back link

--- src/SwatchDetail.js
+++ src/SwatchDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Swatch from './Swatch';
 import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
 import PaletteFooter from './PaletteFooter';
 
 class SwatchDetail extends Component {
@@ -28,15 +29,22 @@ class SwatchDetail extends Component {
 
   render() {
     const { format } = this.state;
-    const {paletteName, emoji} = this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
     const swatches = this._shades.map(swatch => (
-      <Swatch key={swatch.id} name={swatch.name} background={swatch[format]} showLink={false} />
+      <Swatch key={swatch.name} name={swatch.name} background={swatch[format]} showLink={false} />
     ));
     return (
-      <div className="Palette">
+      <div className="SwatchDetail Palette">
         <Navbar handleFormatChange={this.changeFormat} swatchDetail />
-        <div className="Palette-swatches">{swatches}</div>\
-        <PaletteFooter paletteName={paletteName} emoji={emoji}/>
+        <div className="Palette-swatches">
+          {swatches}
+          <div className="go-back Swatch">
+            <Link to={`/palette/${id}`} className="back-button">
+              Go Back
+            </Link>
+          </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
